498de22440fba7ca1af657b2ba615bf39b0547b6
MatthewCoff <miramavs@gmail.com>
2022-12-09 18:29:38 +0200

    Добавьте файлы проекта.

---
+++ гит.cpp
@@ -0,0 +1,53 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+//
+using namespace std;
+class Lambda///це клас функтор
+{
+public:
+
+    void operator ()(int x) const/////
+    {
+        cout << x << " ";
+    }
+};
+void Func(int& x)
+{
+    x += 2;
+    cout << x << " ";
+}
+
+void main()
+{
+    //
+    //
+    int  age = 18;
+    auto otvet = [age]()
+    {
+        int x = 1;
+        return age + x;
+    };
+     
+        vector<int> srcVec;
+        for (int val = 0; val < 10; ++val)
+        {
+            srcVec.push_back(val);
+        }
+    
+        /*vector<int>::iterator it;
+        it = srcVec.begin();
+        it++;*/
+    
+        for_each(srcVec.begin(), srcVec.end(), Func);//предикат
+    
+        cout << endl;
+    
+        for_each(srcVec.begin(), srcVec.end(), Lambda());///Lambda() це стаорення обьєкту функтора/конструктор 
+        ///
+        /// алгоритм for_each буде визивати operator () з класу функтора Lambda
+        /// 
+        cout << endl;
+
+}
+
